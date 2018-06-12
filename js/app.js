ebd151cc1521a97c74bcdbb07dc4c49282f46b47
dezziner93 <40169793+dezziner93@users.noreply.github.com>
2018-06-12 17:44:30 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-// this is javascript file.
+// this is javascrip file.
+
+// Define UI Vars
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
